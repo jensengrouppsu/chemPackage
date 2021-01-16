59be746c34b974fb6eec7ce5004565e11d62850a
jbecca <jeff.becca@gmail.com>
2021-01-16 17:21:43 -0500

    added additional unit tests to ADF class

--- test_suite/unittests/unittest_adf.py
+++ test_suite/unittests/unittest_adf.py
@@ -5,12 +5,16 @@ class TestADFMethods(unittest.TestCase):
     def setUp(self):
         self.test = collect('../adf/raman/raman-benzene.orig.out')
 
+    def test_init(self):
+        self.assertEqual(type(self.test.program), type(str('ADF')), 'incorrect program')
+        self.assertEqual(self.test.program, 'ADF', 'incorrect program')
+        self.assertEqual(self.test.project, 'all', 'incorrect project')
+        self.assertEqual(self.test.filetype, 'out', 'incorrect filetype')
     def test_calctype(self):
         ct_array = {'DFT', 'FREQUENCIES', 'POLARIZABILITY', 'RAMAN', 'STATIC'}
         self.assertEqual(type(self.test.calctype), type(ct_array), 'incorrect calctype type')
         self.assertEqual(len(self.test.calctype), len(ct_array), 'mismatch length')
         self.assertEqual(self.test.calctype, ct_array, 'mismatch string')
         
-
 if __name__ == '__main__':
     unittest.main()
