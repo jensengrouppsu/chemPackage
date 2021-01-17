2da636ff29404e200b7e77fe750be68d22b5d5a4
jbecca <jeff.becca@gmail.com>
2021-01-17 15:13:58 -0500

    added to adf_test.py

--- unitTests/adf_test.py
+++ unitTests/adf_test.py
@@ -10,11 +10,14 @@ class TestADFMethods(unittest.TestCase):
         self.assertEqual(self.test.program, 'ADF', 'incorrect program')
         self.assertEqual(self.test.project, 'all', 'incorrect project')
         self.assertEqual(self.test.filetype, 'out', 'incorrect filetype')
+        self.assertEqual(self.test.filename, 'unitTests/raman-benzene.out', 'incorrect name')
     def test_calctype(self):
         ct_array = {'DFT', 'FREQUENCIES', 'POLARIZABILITY', 'RAMAN', 'STATIC'}
         self.assertEqual(type(self.test.calctype), type(ct_array), 'incorrect calctype type')
         self.assertEqual(len(self.test.calctype), len(ct_array), 'mismatch length')
         self.assertEqual(self.test.calctype, ct_array, 'mismatch string')
-        
+    def test_collect(self):
+        # unit tests for testing things that must be correct in adf collection
+        self.assertEqual(self.test._abort, True, 'incorrect abort setting')
 if __name__ == '__main__':
     unittest.main()
