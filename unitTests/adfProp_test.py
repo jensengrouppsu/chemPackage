d3235b52c34d262a93111e6d4f536ad18868a154
jbecca <jeff.becca@gmail.com>
2021-02-06 22:39:16 -0500

    changed to work with h2o file for easier testing

--- unitTests/adfProp_test.py
+++ unitTests/adfProp_test.py
@@ -4,13 +4,13 @@ import numpy as np
 
 class TestADFMethods(unittest.TestCase):
     def setUp(self):
-        self.test = collect('unitTests/raman-benzene.out')
+        self.test = collect('unitTests/h2o_freq.out')
 
     def test_vFrequencies(self):
-        self.assertEqual(len(self.test.v_frequencies), 30, 'incorrect vFreq length')
-        self.assertEqual(self.test.v_frequencies[0], 411.02, 'incorrect low freq')
-        self.assertEqual(self.test.v_frequencies[17], 1134.248, 'incorrect mid freq')
-        self.assertEqual(self.test.v_frequencies[29], 3137.143, 'incorrect high freq')
+        self.assertEqual(len(self.test.v_frequencies), 3, 'incorrect vFreq length')
+        self.assertEqual(self.test.v_frequencies[0], 1578.216, 'incorrect low freq')
+        self.assertEqual(self.test.v_frequencies[1], 3616.061, 'incorrect mid freq')
+        self.assertEqual(self.test.v_frequencies[2], 3727.173, 'incorrect high freq')
 
 if __name__ == '__main__':
     unittest.main()
