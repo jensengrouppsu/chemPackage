70b953ca46f23e0978d9984d4de1b9de55f3356c
jbecca <jeff.becca@gmail.com>
2021-02-09 22:57:40 -0500

    added in new orbital tests

--- unitTests/adfOrbital_test.py
+++ unitTests/adfOrbital_test.py
@@ -7,6 +7,10 @@ class TestADFMethods(unittest.TestCase):
     def setUp(self):
         self.test = collect('unitTests/h2o_pol.out')
 
-    def test_OrbitalE(self):
+    def test_Orbital(self):
         orbE = np.array([-25.34, -13.259, -9.557, -7.398, -0.228, 2.232])
         np.testing.assert_array_almost_equal(self.test.orbital_energies, orbE)
+        occ = np.array([2., 2., 2., 2., 0., 0.])
+        np.testing.assert_array_almost_equal(self.test.orbital_occ, occ)
+        spin = np.array([0, 0, 0, 0, 0, 0])
+        np.testing.assert_array_almost_equal(self.test.orbital_spin, spin)
