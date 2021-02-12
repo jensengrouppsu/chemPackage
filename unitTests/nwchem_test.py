f90f24ed296189a0ce160698e97ca9793ed8241c
jbecca <jeff.becca@gmail.com>
2021-02-12 16:31:02 -0500

    added nwchem test case for collection of energy

--- unitTests/nwchem_test.py
+++ unitTests/nwchem_test.py
@@ -10,3 +10,11 @@ class TestNWClass(unittest.TestCase):
         self.assertEqual(self.test.program, 'NWChem', 'incorrect program')
         self.assertEqual(self.test.filetype, 'out', 'incorrect filetype')
         self.assertEqual(self.test.filename, 'unitTests/nh3Geom.out', 'incorrect name')
+
+    def test_energy(self):
+        self.assertEqual(self.test.energy['total DFT'], -56.509475409542, 'incorrect energy')
+        self.assertEqual(self.test.energy['one-electron'], -99.473135904705, 'incorrect energy')
+        self.assertEqual(self.test.energy['Coulomb'], 39.146396077606, 'incorrect energy')
+        self.assertEqual(self.test.energy['XC'], -8.026930069048, 'incorrect energy')
+        self.assertEqual(self.test.energy['nuc. repulsion'], 11.844194486605, 'incorrect energy')
+        self.assertEqual(self.test.energy['total'], -56.509475409542, 'incorrect energy')
