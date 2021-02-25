ddf74fb556a5a98ada240ce2857dec51938cd1ac
jbecca <jeff.becca@gmail.com>
2021-02-24 20:39:09 -0500

    added test for TDM and TQM for nwchem

--- unitTests/nwchemExc_test.py
+++ unitTests/nwchemExc_test.py
@@ -23,3 +23,17 @@ class TestNWExc(unittest.TestCase):
     def test_ExcType(self):
         Sym = np.array(['SS', 'SS', 'SS', 'SS'])
         np.testing.assert_array_equal(self.test.excitation_type, Sym)
+
+    def test_TDM(self):
+        TDM = np.array([[ 4.5619e-01, -1.1826e-01, -0.0000e+00],
+                        [-9.4550e-02, -3.6470e-01, -0.0000e+00],
+                        [ 0.0000e+00,  0.0000e+00, -3.7673e-01],
+                        [ 0.0000e+00,  2.0000e-05, -7.9350e-01]])
+        np.testing.assert_array_almost_equal(self.test.TDM, TDM)
+
+    def test_TQM(self):
+        TQM = np.array([[-2.5638e-01, -1.4990e-02,  0.0000e+00, -2.5638e-01, -1.4990e-02, 0.0000e+00],
+                        [ 2.3376e-01,  4.4838e-01,  0.0000e+00,  2.3376e-01,  4.4838e-01, 0.0000e+00],
+                        [-0.0000e+00, -0.0000e+00,  3.8467e-01, -0.0000e+00, -0.0000e+00, 3.8467e-01],
+                        [-1.0000e-05, -1.0000e-05,  3.2939e-01, -1.0000e-05, -1.0000e-05, 3.2939e-01]])
+        np.testing.assert_array_almost_equal(self.test.TQM, TQM)
