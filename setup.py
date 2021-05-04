79f12afbe98542f3085eca785c815a5f65fcb523
jbecca <jeff.becca@gmail.com>
2021-05-04 18:11:08 -0400

    created setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='chemPackage',
+    version='1.1.0',
+    description='Python package for interacting with data from quantum chemistry programs',
+    url='https://github.com/jensengrouppsu/chemPackage',
+    author='Jensen Research Group',
+    author_email='jeff.becca@gmail.com',
+    license='GPL v3.0',
+    packages=['chemPackage'],
+    install_requires=['numpy',
+                      'natsort',
+                      'cPickle',
+                      'math',
+                      'itertools',
+                      'difflib',
+                      'pprint'],
+    classifiers=['Development Status :: 1 - Planning',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+                 'Natural Language :: English',
+                 'Programming Language :: Python :: 3,',
+                 'Topic :: Scientific/Engineering :: Chemistry',
+                 'Topic :: Scientific/Engineering :: Physics',],
+)
